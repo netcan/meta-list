84da724d25b079277caf20e3dcc360ba51479c31
netcan <netcan1996@gmail.com>
2021-11-10 22:53:42 +0800

    add constraint: is_pred_satisfied

--- include/value-list/algo/is_pred_satisfied.hpp
+++ include/value-list/algo/is_pred_satisfied.hpp
@@ -5,10 +5,12 @@
 #ifndef VALUE_LIST_IS_PRED_SATISFIED_HPP
 #define VALUE_LIST_IS_PRED_SATISFIED_HPP
 #include <value-list/types/value_c.hpp>
+#include <value-list/concept/value_or_type.hpp>
 #include <type_traits>
 VALUE_LIST_NS_BEGIN
 template<typename P, typename E>
-inline constexpr bool is_pred_satisfied // TODO: constraint P result is ValueConst
+requires (concepts::value_const<std::invoke_result_t<P, E>>)
+inline constexpr bool is_pred_satisfied
     = std::is_same_v<std::invoke_result_t<P, E>, ValueConst<true>>;
 VALUE_LIST_NS_END
 #endif //VALUE_LIST_IS_PRED_SATISFIED_HPP
